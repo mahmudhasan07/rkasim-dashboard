7356f93fcfd689e2d776104241d425df200ec421
unknown <mahmudhasan.hb@gmail.com>
2025-06-26 16:13:40 +0600

    update base url

--- src/Redux/Api/baseApi.ts
+++ src/Redux/Api/baseApi.ts
@@ -1,21 +1,31 @@
 // src/features/api/baseApi.js
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import Cookies from 'js-cookie';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import Cookies from "js-cookie";
 export const baseApi = createApi({
-    reducerPath: 'baseApi', // The key for this API in the Redux store
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'https://api.mycvconnect.com/api/v1', // Replace with your API's base URL
-        // baseUrl: 'http://localhost:6001/api/v1', // Replace with your API's base URL
-        prepareHeaders: (headers) => {
-            const token = Cookies.get("accessToken") // Assuming token is stored in the auth slice
-            if (token) {
-                headers.set('Authorization', `Bearer ${token}`);
-            }
-            return headers;
-        },
-    }),
-    endpoints: () => ({}),
-    tagTypes: ["approveEvent", "allEvents", "logIn", "transaction", "allUsers", "allCreators", "complains", "updateSubscription", "dashboardInfo"]
+  reducerPath: "baseApi", // The key for this API in the Redux store
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://api.mycvconnect.com/api/v1", // Replace with your API's base URL
+    // baseUrl: 'http://localhost:6001/api/v1', // Replace with your API's base URL
+    prepareHeaders: (headers) => {
+      const token = Cookies.get("accessToken"); // Assuming token is stored in the auth slice
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  endpoints: () => ({}),
+  tagTypes: [
+    "approveEvent",
+    "allEvents",
+    "logIn",
+    "transaction",
+    "allUsers",
+    "allCreators",
+    "complains",
+    "updateSubscription",
+    "dashboardInfo",
+  ],
 });
 
 // Export hooks for usage in functional components
